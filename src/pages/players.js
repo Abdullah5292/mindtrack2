9f497c3d5d42c79abaf95d254d664e8071396946
Abdullah5292 <112702464+Abdullah5292@users.noreply.github.com>
2025-05-01 23:19:07 +0500

    Player in progress

--- src/pages/players.js
+++ src/pages/players.js
@@ -100,10 +100,10 @@ const Page = (props) => {
           <Stack spacing={3}>
             <Stack direction="row" justifyContent="space-between" spacing={4}>
               <Stack spacing={1}>
-                <Typography variant="h4" sx={{ color: "white", zIndex: 140 }}>Users</Typography>
+                <Typography variant="h4" sx={{ color: "white", zIndex: 140 }}>Players</Typography>
               </Stack>
               <div>
-                <Button
+                {/* <Button
                   startIcon={
                     <SvgIcon fontSize="small">
                       <PlusIcon />
@@ -132,7 +132,7 @@ const Page = (props) => {
                   }}
                 >
                   Add User
-                </Button>
+                </Button> */}
 
               </div>
             </Stack>
