cd3ee1228c083b45a630d66412b4afe388da709b
hammad1029 <hammad1029@gmail.com>
2025-06-03 23:04:17 +0500

    ...

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   reactStrictMode: true,
   output: "standalone",
   publicRuntimeConfig: {
-    backend: process.env.NEXT_PUBLIC_BACKEND,
+    backend: process.env.BACKEND,
     awsS3Bucket: process.env.AWS_S3_BUCKET,
     awsRegion: process.env.AWS_REGION,
     awsAccessKey: process.env.AWS_ACCESS_KEY_ID,
